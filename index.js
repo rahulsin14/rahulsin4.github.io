19e64bf9a25a6d535ee88d2f2db38e21468a460b
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-02-24 07:01:44 +0530

    added ejs as view engine and path has been set for views

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8100;
 app.use('/',require('./routes'));
+app.set('view_engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`error:${err}`);
